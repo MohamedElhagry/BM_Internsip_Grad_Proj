cac4c288d35a409f1905d32c352832d3563e94a9
Mohamed <mohamedelhagery@gmail.com>
2022-08-17 21:19:31 +0200

    trying to fix CORS error again 7

--- src/main/java/com/project/bm_internsip_grad_project/securityConfig/SecurityConfiguration.java
+++ src/main/java/com/project/bm_internsip_grad_project/securityConfig/SecurityConfiguration.java
@@ -4,7 +4,6 @@ package com.project.bm_internsip_grad_project.securityConfig;
 import com.project.bm_internsip_grad_project.securityFilters.CustomAuthenticationFilter;
 import com.project.bm_internsip_grad_project.securityFilters.CustomAuthorizationFilter;
 import com.project.bm_internsip_grad_project.services.UserService;
-import org.apache.catalina.filters.CorsFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,8 +18,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
-//import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
 
@@ -43,28 +41,27 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     // http://localhost:8081/swagger-ui/index.html#/
     // to view the docs
     private static final String[] whiteList =
-    {
-            "/auth/register",
-            "/login",
-            "/auth/login",
-            "/auth/users",
-            "/swagger-ui/**",
-            "/v2/api-docs",
-            "/swagger-ui/",
-            "/v3/api-docs/**"
-    };
+            {
+                    "/auth/register",
+                    "/login",
+                    "/auth/login",
+                    "/auth/users",
+                    "/swagger-ui/**",
+                    "/v2/api-docs",
+                    "/swagger-ui/",
+                    "/v3/api-docs/**"
+            };
 
     private static final String[] allowedURLS =
-    {
-            "https://murmuring-temple-54993.herokuapp.com",
-            "http://localhost:8081",
-//            "http://localhost:47"
-    };
+            {
+                    "https://murmuring-temple-54993.herokuapp.com",
+                    "http://localhost:8081"
+            };
 
     private static final String[] userPages=
-    {
+            {
 
-    };
+            };
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -75,17 +72,12 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(authenticationManagerBean());
         customAuthenticationFilter.setFilterProcessesUrl("/auth/login");
-        http.authorizeRequests().antMatchers(whiteList).permitAll().anyRequest()
-                .authenticated().and().addFilter(customAuthenticationFilter)
-                .addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class)
-                .sessionManagement().sessionCreationPolicy(STATELESS)
-                .and().cors().and().csrf().disable();
-//        http.cors().and().csrf().disable();
-//        http.sessionManagement().sessionCreationPolicy(STATELESS);
-////        http.authorizeRequests().antMatchers()
-//        http.authorizeRequests().antMatchers(whiteList).permitAll().anyRequest().authenticated();
-//        http.addFilter(customAuthenticationFilter);
-//        http.addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
+        http.csrf().disable();
+        http.sessionManagement().sessionCreationPolicy(STATELESS);
+//        http.authorizeRequests().antMatchers()
+        http.authorizeRequests().antMatchers(whiteList).permitAll().anyRequest().authenticated();
+        http.addFilter(customAuthenticationFilter);
+        http.addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 
     @Bean
@@ -98,19 +90,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         source.registerCorsConfiguration("/**", configuration);
         return source;
     }
-
-//
-//    @Bean
-//    public CorsFilter corsFilter() {
-//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//        CorsConfiguration config = new CorsConfiguration();
-//        config.addAllowedOrigin("*");
-//        config.addAllowedHeader("*");
-//        config.addAllowedMethod("*");
-//        source.registerCorsConfiguration("/**", config);
-//        CorsFilter bean = new CorsFilter(source);
-//        return bean;
-//    }
 
     @Bean
     @Override
--- src/main/java/com/project/bm_internsip_grad_project/securityConfig/WebConfiguration.java
+++ src/main/java/com/project/bm_internsip_grad_project/securityConfig/WebConfiguration.java
@@ -1,14 +1,14 @@
-package com.project.bm_internsip_grad_project.securityConfig;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class WebConfiguration implements WebMvcConfigurer {
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedMethods("*");
-    }
-}
+//package com.project.bm_internsip_grad_project.securityConfig;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//public class WebConfiguration implements WebMvcConfigurer {
+//
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**").allowedMethods("*");
+//    }
+//}

55c074147675b8310a3bd377679791c661b3c19b
Mohamed <mohamedelhagery@gmail.com>
2022-08-17 20:58:06 +0200

    trying to fix CORS error again 3

--- src/main/java/com/project/bm_internsip_grad_project/securityConfig/SecurityConfiguration.java
+++ src/main/java/com/project/bm_internsip_grad_project/securityConfig/SecurityConfiguration.java
@@ -4,6 +4,7 @@ package com.project.bm_internsip_grad_project.securityConfig;
 import com.project.bm_internsip_grad_project.securityFilters.CustomAuthenticationFilter;
 import com.project.bm_internsip_grad_project.securityFilters.CustomAuthorizationFilter;
 import com.project.bm_internsip_grad_project.services.UserService;
+import org.apache.catalina.filters.CorsFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,7 +19,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+//import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
 
@@ -55,7 +57,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     private static final String[] allowedURLS =
     {
             "https://murmuring-temple-54993.herokuapp.com",
-            "http://localhost:8081"
+            "http://localhost:8081",
+
     };
 
     private static final String[] userPages=
@@ -72,24 +75,42 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(authenticationManagerBean());
         customAuthenticationFilter.setFilterProcessesUrl("/auth/login");
-        http.csrf().disable();
-        http.sessionManagement().sessionCreationPolicy(STATELESS);
-//        http.authorizeRequests().antMatchers()
-        http.authorizeRequests().antMatchers(whiteList).permitAll().anyRequest().authenticated();
-        http.addFilter(customAuthenticationFilter);
-        http.addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
+        http.authorizeRequests().antMatchers(whiteList).permitAll().anyRequest()
+                .authenticated().and().addFilter(customAuthenticationFilter)
+                .addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class)
+                .sessionManagement().sessionCreationPolicy(STATELESS)
+                .and().cors().and().csrf().disable();
+//        http.cors().and().csrf().disable();
+//        http.sessionManagement().sessionCreationPolicy(STATELESS);
+////        http.authorizeRequests().antMatchers()
+//        http.authorizeRequests().antMatchers(whiteList).permitAll().anyRequest().authenticated();
+//        http.addFilter(customAuthenticationFilter);
+//        http.addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 
-    @Bean
-    public UrlBasedCorsConfigurationSource corsConfigurationSource()
-    {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList(allowedURLS));
-        configuration.setAllowedMethods(Arrays.asList("GET","POST"));
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
-    }
+//    @Bean
+//    public UrlBasedCorsConfigurationSource corsConfigurationSource()
+//    {
+//        CorsConfiguration configuration = new CorsConfiguration();
+//        configuration.setAllowedOrigins(Arrays.asList(allowedURLS));
+//        configuration.setAllowedMethods(Arrays.asList("GET","POST"));
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/**", configuration);
+//        return source;
+//    }
+
+//
+//    @Bean
+//    public CorsFilter corsFilter() {
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        CorsConfiguration config = new CorsConfiguration();
+//        config.addAllowedOrigin("*");
+//        config.addAllowedHeader("*");
+//        config.addAllowedMethod("*");
+//        source.registerCorsConfiguration("/**", config);
+//        CorsFilter bean = new CorsFilter(source);
+//        return bean;
+//    }
 
     @Bean
     @Override
